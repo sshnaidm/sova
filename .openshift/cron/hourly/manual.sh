dee9df12e301579f7f8a205cea53c8546720cc6c
Sagi Shnaidman <sshnaidm@redhat.com>
2016-06-09 16:14:56 +0300

    Fix cron job logs

--- .openshift/cron/hourly/manual.sh
+++ .openshift/cron/hourly/manual.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$OPENSHIFT_PYTHON_DIR/virtenv/venv/bin/python $OPENSHIFT_REPO_DIR/manual.py 2>&1 > $OPENSHIFT_DATA_DIR/log.log
+$OPENSHIFT_PYTHON_DIR/virtenv/venv/bin/python $OPENSHIFT_REPO_DIR/manual.py &> $OPENSHIFT_DATA_DIR/log.log
