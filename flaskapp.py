2da79431437e15954957bc947f880fee515646e2
Sagi Shnaidman <sshnaidm@redhat.com>
2016-06-19 18:15:32 +0300

    Fix flask app file

--- flaskapp.py
+++ flaskapp.py
@@ -1,15 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 
 from tripleoci import config
 app = Flask(__name__)
 
 work_dir = os.path.dirname(__file__)
 
+
 @app.route('/')
 def hello_world():
-    with open(config.INDEX_HTML) as f:
-        return f.read()
+    return send_from_directory(
+        os.path.dirname(config.INDEX_HTML), 'index.html')
 
 if __name__ == '__main__':
     app.run()
