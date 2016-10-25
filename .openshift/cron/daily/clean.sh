e689f40947b2a70c38d20de8d03830b13d98ea9a
Sagi Shnaidman <sshnaidm@redhat.com>
2016-10-25 14:31:42 +0300

    Log for clean script

--- .openshift/cron/daily/clean.sh
+++ .openshift/cron/daily/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $OPENSHIFT_DATA_DIR -type d -mtime +20 -exec rm -rf "{}" \;
+find $OPENSHIFT_DATA_DIR -type d -mtime +20 -exec rm -rf "{}" \; 2>&1| tee $OPENSHIFT_DATA_DIR/clean.log
